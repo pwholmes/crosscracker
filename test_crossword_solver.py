d91751613476c40b18b5aad3e7757d0654931a7d
Paul Holmes <pwholmes151@gmail.com>
2025-05-23 09:19:02 -0400

    Update confidence scores in test_crossword_solver.py

--- test_crossword_solver.py
+++ test_crossword_solver.py
@@ -21,16 +21,16 @@ SOLVED_TEST_GRID = [
 
 # Clues for this sample grid
 TEST_CLUES = [
-    Clue(1, 'A', "One of a matched pair", 4, 0, 1, [("SOCK", 75), ("TWIN", 60)]),
-    Clue(5, 'A', "Exclamation of surprise", 4, 1, 1, [("OHMY", 100)]),
-    Clue(6, 'A', "Fruit used to make wine", 5, 2, 0, [("GRAPE", 100)]),
-    Clue(7, 'A', "Land surrounded by water", 4, 3, 0, [("ISLE", 100)]),
+    Clue(1, 'A', "One of a matched pair", 4, 0, 1, [("SOCK", 70), ("TWIN", 60)]),
+    Clue(5, 'A', "Exclamation of surprise", 4, 1, 1, [("OHMY", 70), ("DAMN", 60)]),
+    Clue(6, 'A', "Fruit used to make wine", 5, 2, 0, [("GRAPE", 90), ("APPLE", 50)]),
+    Clue(7, 'A', "Land surrounded by water", 4, 3, 0, [("ISLE", 80), ("BANK", 50)]),
     Clue(8, 'A', "Christmas song subject", 4, 4, 0, [("SNOW", 90), ("NOEL", 70)]),
-    Clue(1, 'D', "Human trunk sans limbs and head", 5, 0, 1, [("TORSO", 100)]),
-    Clue(2, 'D', "Giant marine mammal", 5, 0, 2, [("WHALE", 100)]),
-    Clue(3, 'D', "Drive or urge onward", 5, 0, 3, [("IMPEL", 100)]),
+    Clue(1, 'D', "Human trunk sans limbs and head", 5, 0, 1, [("TORSO", 80), ("BELLY", 50)]),
+    Clue(2, 'D', "Giant marine mammal", 5, 0, 2, [("WHALE", 90), ("OTTER", 20)]),
+    Clue(3, 'D', "Drive or urge onward", 5, 0, 3, [("IMPEL", 80), ("FORCE", 50)]),
     Clue(4, 'D', "Science Guy Bill", 3, 0, 4, [("NYE", 100)]),
-    Clue(6, 'D', "Spirit in a martini", 3, 2, 0, [("GIN", 100)])
+    Clue(6, 'D', "Spirit in a martini", 3, 2, 0, [("GIN", 90), ("RUM", 50)])
 ]
 
 # Create and print the sample grid for verification
