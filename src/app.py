71df7cafe0f353d4bd76cc1c1adfe6c91b4df0a3
Paul Holmes <pwholmes151@gmail.com>
2025-05-23 09:16:44 -0400

    Refactor app.py to import test data from test_crossword_solver.py instead of duplicating it

--- src/app.py
+++ src/app.py
@@ -1,38 +1,10 @@
 from flask import Flask, render_template, jsonify
 from typing import List, Dict, Any, Optional, Set
 from src.crossword_solver import CrosswordSolver, Clue, Grid
+# Import test data from test_crossword_solver.py
+from test_crossword_solver import EMPTY_TEST_GRID, TEST_CLUES
 
 app = Flask(__name__)
-
-# Sample data from test_crossword_solver.py
-EMPTY_TEST_GRID = [
-    "#****",
-    "#****",
-    "*****",
-    "****#",
-    "****#",
-]
-
-SOLVED_TEST_GRID = [
-    "#TWIN",
-    "#OHMY",
-    "GRAPE",
-    "ISLE#",
-    "NOEL#",
-]
-
-TEST_CLUES = [
-    Clue(1, 'A', "One of a matched pair", 4, 0, 1, [("SOCK", 70), ("TWIN", 60)]),
-    Clue(5, 'A', "Exclamation of surprise", 4, 1, 1, [("OHMY", 70), ("DAMN", 60)]),
-    Clue(6, 'A', "Fruit used to make wine", 5, 2, 0, [("GRAPE", 90), ("APPLE", 50)]),
-    Clue(7, 'A', "Land surrounded by water", 4, 3, 0, [("ISLE", 80), ("BANK", 50)]),
-    Clue(8, 'A', "Christmas song subject", 4, 4, 0, [("SNOW", 90), ("NOEL", 70)]),
-    Clue(1, 'D', "Human trunk sans limbs and head", 5, 0, 1, [("TORSO", 80), ("BELLY", 50)]),
-    Clue(2, 'D', "Giant marine mammal", 5, 0, 2, [("WHALE", 90), ("OTTER", 20)]),
-    Clue(3, 'D', "Drive or urge onward", 5, 0, 3, [("IMPEL", 80), ("FORCE", 50)]),
-    Clue(4, 'D', "Science Guy Bill", 3, 0, 4, [("NYE", 100)]),
-    Clue(6, 'D', "Spirit in a martini", 3, 2, 0, [("GIN", 90), ("RUM", 50)])
-]
 
 # Global variables to store the solver state
 solver: Optional[CrosswordSolver] = None
